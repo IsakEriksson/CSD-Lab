043a56bd3104190ef90b6f6bc06ce184b0b99e7a
IsakEriksson <isak1993@gmail.com>
2018-04-09 18:19:30 +0200

    Just commit please (prease)

---
+++ CSD_Lab_IsakEriksson/PersonSearcher.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSD_Lab_IsakEriksson
+{
+    //This should later on inherit basic search functions from a superclass
+    class PersonSearcher : Searcher<Person>
+    {
+        PersonStorage storage { get; set; }
+
+        public PersonSearcher(PersonStorage storage)
+        {
+            this.storage = storage;
+        }
+
+        public List<Person> NameSearch(string name)
+        {
+            List<Person> results;
+            results = BasicSearch(storage, "GetFirstName", name);
+            results = BasicSearch(storage, "GetLastName", name);
+            return results;
+        }
+
+        /*public List<Person> NameSearch(string name)
+        {
+            List<Person> results = new List<Person>();
+            foreach (Person person in storage)
+            {
+                if ((person.GetFirstName() == name) || (person.GetLastName() == name) || person.GetFirstName().Contains(name) || person.GetLastName().Contains(name))
+                {
+                    results.Add(person);
+                }
+            }
+            return results;
+        }
+
+        public List<Person> NumberSearch(string number)
+        {
+            List<Person> results = new List<Person>();
+            foreach (Person person in storage)
+            {
+                if ((person.GetPhoneNumber() == number) || person.GetPhoneNumber().Contains(number))
+                {
+                    results.Add(person);
+                }
+            }
+            return results;
+        }*/
+    }
+}
---
+++ CSD_Lab_IsakEriksson/Searcher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace CSD_Lab_IsakEriksson
+{
+    class Searcher<T>
+    {
+        List<T> results = new List<T>();
+        public List<T> BasicSearch(IStorage<T> storage, string function, string searchString)
+        {
+            Type thistype = typeof(T);
+            MethodInfo method = thistype.GetMethod(function);
+            foreach (T item in storage)
+            {
+                string something = method.Invoke(item, null).ToString();
+                Console.WriteLine(something);
+                if (something == searchString) 
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+    }
+}
